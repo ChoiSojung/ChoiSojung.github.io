2b3e29be6fb62cfbaeeb93aa73278a9a58535789
[choisojung] <[sojc31@gmail.com]>
2019-01-16 21:11:33 -0800

    updated button script to print to console

--- myScript.js
+++ myScript.js
@@ -1,16 +1,8 @@
-
-function button() {
-document.getElementById("myBtn").style.display = "block";
-    
-}
-
-button();
-
-// When the user clicks on the button, show date/time stamp
+// When function is called, return date/time stamp
 function dateStamp() {
     var d = new Date();
     alert(d);
-    document.getElementById("date").innerHTML =d;
+    console.log(d);
 }
 
 
